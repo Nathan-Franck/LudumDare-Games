76fedbe7e4f25a43c56b45aa1bcc51c477624e70
Nathan Franck <nathanfranck1@gmail.com>
2024-04-13 17:00:24 -0600

    Alpha blending works finally!

--- LudumDare55-Summoning/web-src/app.tsx
+++ LudumDare55-Summoning/web-src/app.tsx
@@ -1,5 +1,5 @@
 import { declareStyle } from './declareStyle';
-import { useEffect, useRef } from 'preact/hooks'
+import { useEffect, useRef, useState } from 'preact/hooks'
 import { sliceToArray, callWasm } from './zigWasmInterface';
 import { ShaderBuilder } from "./shaderBuilder";
 
@@ -11,6 +11,7 @@ const startTime = Date.now();
 
 export function App() {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const [framerate, setFramerate] = useState(0);
 
     useEffect(() => {
     }, []);
@@ -32,19 +33,17 @@ export function App() {
                 modelViewMatrix: { type: "uniform", unit: "mat4", count: 1 },
                 texture: { type: "uniform", unit: "sampler2D", count: 1 },
                 uv: { type: "varying", unit: "vec2" },
-				modelPosition: {
-					type: "attribute",
-					unit: "vec3",
-					instanced: true,
-				},
+                modelPosition: { type: "attribute", unit: "vec3", instanced: true },
             },
             vertSource: `
+                precision highp float;
                 void main(void) {
-                    gl_Position = modelViewMatrix * meshVertexPosition + vec4(modelPosition, 1.0);
+                    gl_Position = modelViewMatrix * meshVertexPosition  + vec4(modelPosition, 1.0);
                     uv = meshVertexUV;
                 }
             `,
             fragSource: `
+                precision highp float;
                 void main(void) {
                     gl_FragColor = texture2D(texture, uv);
                 }
@@ -52,7 +51,6 @@ export function App() {
         });
         const meshTriangle = ShaderBuilder.createElementBuffer(gl, new Uint16Array([
             0, 1, 2,
-            2, 1, 0,
             0, 2, 3
         ]));
         const meshVertexPosition = ShaderBuilder.createBuffer(gl, new Float32Array([
@@ -64,8 +62,8 @@ export function App() {
         const meshVertexUV = ShaderBuilder.createBuffer(gl, new Float32Array([
             0, 0,
             1, 0,
-            0, 1,
-            1, 1
+            1, 1,
+            0, 1
         ]));
         const modelPosition = ShaderBuilder.createBuffer(gl, new Float32Array([
             0, 0, 0
@@ -81,6 +79,8 @@ export function App() {
                 gl.enable(gl.DEPTH_TEST);
                 gl.clear(gl.COLOR_BUFFER_BIT);
                 gl.viewport(0, 0, canvas.width, canvas.height);
+                gl.enable(gl.BLEND);
+                gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
             }
 
             const time = Date.now() - startTime;
@@ -96,7 +96,16 @@ export function App() {
                 modelViewMatrix: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
             });
         }
+        let lastTime = Date.now();
+        let frameCount = 0;
         const loop = () => {
+            const time = Date.now();
+            frameCount++;
+            if (time - lastTime > 1000) {
+                setFramerate(frameCount);
+                frameCount = 0;
+                lastTime = time;
+            }
             updateAndRender();
             requestAnimationFrame(loop);
         }
@@ -105,6 +114,7 @@ export function App() {
 
     return (
         <>
+            <div class={classes.frameRate}>Frame Rate: {framerate}</div>
             <style>{encodedStyle}</style>
             <canvas ref={canvasRef} id="canvas" width="540" height="480"></canvas>
         </>
